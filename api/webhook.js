770c452ce6f438a6ec34811c47d32967055f0519
knockoutperri <knockoutperri@gmail.com>
2025-04-19 23:32:37 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,3 @@
-import menuData from './menuData.js';
 import OpenAI from 'openai';
 import twilio from 'twilio';
 
@@ -56,6 +55,172 @@ Reglas especiales:
 8. Si el dueño manda mensaje, interpretalo como instrucción para modificar algo.
 
 Horario de atención: 11:30 a 14:30 y 19:00 a 23:59
+
+Este es el menú completo:
+PIZZAS COMUNES:
+
+Muzzarella: Chica $8600 / Grande $12000 / Gigante $25800 / Para cocinar $9700
+Doble Muzza: Chica $11500 / Grande $16000 / Gigante $33200 / Para cocinar $13000
+Muzzarella y Tomate Natural: Chica $8900 / Grande $12500 / Gigante $28800 / Para cocinar $10800
+Muzzarella y Morrón: Chica $8900 / Grande $12500 / Gigante $28800 / Para cocinar $10800
+Muzzarella y Huevo: Chica $8900 / Grande $12500 / Gigante $28800 / Para cocinar $10800
+Muzzarella y Anchoas: Chica $10800 / Grande $16000 / Gigante $34500 / Para cocinar $12500
+Salsa y Anchoas: Chica $8600 / Grande $12000 / Gigante $25800 / Para cocinar $9700
+Ollio: Chica $8600 / Grande $12000 / Gigante $25800 / Para cocinar $9700
+Jamón: Chica $9900 / Grande $13500 / Gigante $30000 / Para cocinar $11000
+Jamón y Morrón: Chica $11000 / Grande $16000 / Gigante $34500 / Para cocinar $12500
+Jamón y Huevo: Chica $11000 / Grande $16000 / Gigante $34500 / Para cocinar $12500
+Primavera: Chica $11200 / Grande $17000 / Gigante $36000 / Para cocinar $13500
+Napolitana: Chica $9900 / Grande $13500 / Gigante $30000 / Para cocinar $11000
+Napolitana con Jamón: Chica $11000 / Grande $16000 / Gigante $34500 / Para cocinar $12500
+Napolitana con Anchoas: Chica $12000 / Grande $17500 / Gigante $37000 / Para cocinar $15000
+Provolone: Chica $11200 / Grande $17000 / Gigante $36000 / Para cocinar $13500
+Provolone con Jamón: Chica $12800 / Grande $18000 / Gigante $36000 / Para cocinar $13500
+Roquefort: Chica $12800 / Grande $18000 / Gigante $37500 / Para cocinar $15500
+Roquefort con Jamón: Chica $13800 / Grande $19500 / Gigante $43500 / Para cocinar $16500
+Calabresa: Chica $10500 / Grande $16500 / Gigante $35500 / Para cocinar $13200
+Calabresa con Provolone: Chica $13700 / Grande $19500 / Gigante $43500 / Para cocinar $16500
+Palmitos: Chica $13500 / Grande $19000 / Gigante $44000 / Para cocinar $16500
+Palmitos con Jamón: Chica $15000 / Grande $21500 / Gigante $46500 / Para cocinar $18900
+Palmitos con Jamón y Morrón: Chica $17500 / Grande $25000 / Gigante $51000 / Para cocinar $21000
+Palmitos con Jamón y Huevo: Chica $17500 / Grande $25000 / Gigante $51000 / Para cocinar $21000
+Palmitos con Jamón, Morrón y Huevo: Chica $18900 / Grande $26700 / Gigante $56000 / Para cocinar $23700
+Fugazza: Chica $8600 / Grande $12000 / Gigante $25800 / Para cocinar $9700
+Fugazzeta: Chica $9900 / Grande $13500 / Gigante $30000 / Para cocinar $11000
+Fugazzeta con Jamón: Chica $10800 / Grande $15000 / Gigante $34000 / Para cocinar $12700
+Ananá con Jamón: Chica $12000 / Grande $17500 / Gigante $36700 / Para cocinar $15000
+
+PIZZAS ESPECIALES:  
+- Super Ananá: Chica $13800 / Grande $19500 / Gigante $44000 / Para cocinar $17000  
+- Super Calabresa: Chica $13800 / Grande $19500 / Gigante $44000 / Para cocinar $17000  
+- Super Napolitana: Chica $14500 / Grande $20000 / Gigante $46000 / Para cocinar $18000  
+- Jamón Crudo y Rúcula: Chica $16500 / Grande $23800 / Gigante $53200 / Para cocinar $21900  
+- Jamón Crudo, Rúcula, Nuez y Roquefort: Chica $19500 / Grande $28000 / Gigante $57000 / Para cocinar $24500  
+- Verdura: Chica $12000 / Grande $17500 / Gigante $37000 / Para cocinar $15000  
+- Vegetariana: Chica $12000 / Grande $17500 / Gigante $37000 / Para cocinar $15000  
+- 3 Quesos: Chica $15000 / Grande $22000 / Gigante $51000 / Para cocinar $19500  
+- 4 Quesos: Chica $18000 / Grande $24500 / Gigante $55000 / Para cocinar $22000  
+- Pollo: Chica $14000 / Grande $20000 / Gigante $46800 / Para cocinar $17500  
+- Ají y Morrón: Chica $11000 / Grande $15000 / Gigante $33000 / Para cocinar $12800  
+- Panceta: Chica $12800 / Grande $18000 / Gigante $42500 / Para cocinar $16000  
+- Panceta y Morrón: Chica $14000 / Grande $20000 / Gigante $46800 / Para cocinar $17000  
+- Panceta y Albahaca: Chica $14000 / Grande $20000 / Gigante $46800 / Para cocinar $17000  
+- Choclo: Chica $12000 / Grande $17500 / Gigante $37000 / Para cocinar $15000  
+- Capresse: Chica $12000 / Grande $17500 / Gigante $37000 / Para cocinar $15000  
+- Cochina: Chica $15000 / Grande $21500 / Gigante $48000 / Para cocinar $18500  
+- Salchicha: Chica $11000 / Grande $15000 / Gigante $34500 / Para cocinar $12800  
+- Zuquini: Chica $12000 / Grande $17500 / Gigante $37000 / Para cocinar $15000  
+- Knock Out: Chica $16500 / Grande $23800 / Gigante $55500 / Para cocinar $21000  
+- Super Knock Out: Chica $22000 / Grande $31500 / Gigante $67000 / Para cocinar $28800  
+- La Gran Knock Out: Chica $22000 / Grande $31500 / Gigante $67000 / Para cocinar $28800  
+
+PIZZAS RELLENAS:  
+- Fugazzeta rellena: Grande $19500  
+- Fugazzeta con Jamón rellena: Grande $21500  
+- Fugazzeta con Jamón y Tomate rellena: Grande $23000  
+- Fugazzeta con Panceta rellena: Grande $26000  
+- Palmitos con Jamón rellena: Grande $26000  
+- Roquefort con Jamón rellena: Grande $28000  
+
+FAINÁ:  
+- Fainá: $1400  
+- Fainá con Muzzarella: $2000  
+
+CALZONES:  
+- Calzón Napolitano: $15000  
+- Calzón Jamón y Morrón: $16000  
+- Calzón Calabresa: $18000  
+- Calzón Primavera: $18000  
+- Calzón Capresse: $18000  
+- Calzón Jamón y Palmitos: $20000  
+- Calzón Roquefort: $20000  
+
+MILANESAS:  
+- Milanesa Sola: Chica $9100 / Mediana $18200 / Grande $27300  
+- Milanesa a Caballo: Chica $11500 / Mediana $23000 / Grande $34500  
+- Milanesa Napolitana: Chica $14000 / Mediana $28000 / Grande $42000  
+- Milanesa Fugazzeta: Chica $10800 / Mediana $21600 / Grande $32400  
+- Milanesa Roquefort: Chica $14400 / Mediana $28800 / Grande $43200  
+- Milanesa Rúcula y Parmesano: Chica $11500 / Mediana $23000 / Grande $34500  
+- Milanesa 3 Quesos: Chica $17300 / Mediana $34600 / Grande $51900  
+- Milanesa Choclo: Chica $10900 / Mediana $21800 / Grande $32700  
+- Milanesa Verdura y Salsa Blanca: Chica $10900 / Mediana $21800 / Grande $32700  
+
+TARTAS:  
+- Verdura: $4300  
+- Jamón y Queso: $5800  
+- Zapallito: $4300  
+- Calabaza: $4300  
+- Combinada: $4300  
+- Choclo: $4300  
+- Primavera: $6500  
+
+TORTILLAS:  
+- Tortilla de Papa: $10000  
+- Papa, Cebolla y Morrón: $11500  
+- Papa, Cebolla, Morrón y Muzza: $13000  
+- Papa, Jamón y Muzza: $14000  
+- Española: $14000  
+
+EMPANADAS:  
+- Carne: Unidad $1800 / Docena $20000  
+- Carne Cuchillo: Unidad $1800 / Docena $20000  
+- Pollo: Unidad $1800 / Docena $20000  
+- Jamón y Queso: Unidad $1800 / Docena $20000  
+- Humita: Unidad $1800 / Docena $20000  
+- Roquefort: Unidad $1800 / Docena $20000  
+- Cebolla y Queso: Unidad $1800 / Docena $20000  
+- Verdura: Unidad $1800 / Docena $20000  
+- Capresse: Unidad $1800 / Docena $20000  
+- Panceta y Morrón: Unidad $1800 / Docena $20000  
+- Panceta y Ciruela: Unidad $1800 / Docena $20000  
+- Apio, Roquefort y Nuez: Unidad $1800 / Docena $20000  
+
+CANASTITAS:  
+- Jamón y Muzzarella: $2800  
+- Panceta y Muzzarella: $2800  
+- Roquefort: $2800  
+- Calabresa: $2800  
+- Tomate seco: $2800  
+- Caprese: $2800  
+- Muzzarella y Cebolla: $2800  
+- Jamón crudo y Rúcula: $2800  
+- 4 Quesos: $2800  
+- Calabaza: $2800  
+
+BEBIDAS:  
+Gaseosas:  
+- Coca-Cola 500ml: $2000  
+- Coca-Cola 1.75L: $3500  
+- Fanta 500ml: $2000  
+- Fanta 1.75L: $3500  
+- Sprite 500ml: $2000  
+- Sprite 1.75L: $3500  
+
+Aguas saborizadas:  
+- Levite Naranja 500ml: $1500  
+- Levite Naranja 1.5L: $3000  
+- Levite Manzana 500ml: $1500  
+- Levite Manzana 1.5L: $3000  
+- Levite Pomelo 500ml: $1500  
+- Levite Pomelo 1.5L: $3000  
+- Levite Pera 500ml: $1500  
+- Levite Pera 1.5L: $3000  
+
+Agua mineral:  
+- Agua Mineral 500ml: $1200  
+- Agua Mineral 1.5L: $2200  
+
+Cervezas:  
+- Heineken 500ml: $3000  
+- Sol 500ml: $3000  
+- Grolsch 500ml: $3000  
+- Imperial IPA 500ml: $2400  
+- Imperial Stout 500ml: $2400  
+- Golden 500ml: $2400  
+- Imperial Lager 500ml: $2400  
+- Miller 500ml: $2400  
+- Corona 710ml: $5500  
 
 DESCRIPCIONES IMPORTANTES: (si te preguntan "ingredientes", o "que lleva la pizza primavera (por ejemplo)). No aclares si no te preguntan
 
@@ -141,20 +306,13 @@ https://drive.google.com/file/d/1nWPxJQPft7MYvqe5SOI1lRhGVPmXdNms/view</Message>
 
   historial.push({
     role: 'user',
-    content: esPrimerMensaje ? `${saludo}. ${mensaje}` : mensaje
+    content: esPrimerMensaje ? ${saludo}. ${mensaje} : mensaje
   });
 
-const mensajes = [
-  {
-    role: 'system',
-    content: `${PROMPT_MAESTRO}
-
-Este es el menú completo (productos, precios y categorías), usalo para responder las preguntas del cliente:
-${JSON.stringify(menuData)}`
-  },
-  ...historial,
-];
-
+  const mensajes = [
+    { role: 'system', content: PROMPT_MAESTRO },
+    ...historial,
+  ];
 
   try {
     const completion = await openai.chat.completions.create({
